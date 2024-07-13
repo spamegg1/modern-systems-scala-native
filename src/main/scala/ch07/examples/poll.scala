358918ed6a1f060ee450003924f34c598749c716
spamegg1 <spamegg@pm.me>
2024-07-13 11:39:52 +0300

    ch07 more examples - poll.scala

--- src/main/scala/ch07/examples/poll.scala
+++ src/main/scala/ch07/examples/poll.scala
@@ -1,11 +1,15 @@
 package ch07
 package examples
 
-import scalanative.unsafe.{extern, Ptr, CFuncPtr3}
+import scalanative.unsafe.{extern, link, Ptr, CFuncPtr3}
+
+// This code is not meant to compile and run, it's conceptual pseudocode.
+// I just made up some stuff to make it compile.
 
 @link("uv")
 @extern
 object Poll:
+  type Loop = ExecutionContext // made it up!
   type PollHandle = Ptr[Ptr[Byte]]
   type PollCB = CFuncPtr3[PollHandle, Int, Int, Unit]
 
